e40e7a222915933c1f10cc7638108f46ec5db31a
SSripilaipong <SHSnail@mail.com>
2025-04-16 00:46:12 +0700

    add default code for ide

--- ide/ide.js
+++ ide/ide.js
@@ -11,8 +11,31 @@ document.addEventListener("keydown", (e) => {
 
 const RULE_KEY = "muto.rules";
 
+
+const defaultCode = `
+((map F B) (H)) = B
+(map F (G Ys...)) (H X Xs...) = (map F (G Ys... (F X))) (H Xs...)
+map F A = (map F ($)) A
+
+((filter P B) (H)) = B
+(filter P B) (H X Xs...) = (filter-case P B (H Xs...)) (P X) X
+(filter-case P (G Ys...) A) true X = (filter P (G Ys... X)) A
+(filter-case P B A) false X = (filter P B) A
+filter P A = (filter P ($)) A
+
+((fold F Z) (G)) = Z
+(fold F Z) (G X Xs...) = (fold F (F Z X)) (G Xs...)
+fold F Z A = (fold F Z) A
+
+(curry F S...) X... = F S... X...
+
+
+
+try-this-in-the-query = (filter string? ($ 1 "2" 3 4 "5"))
+`.trim()
+
 // load + save code
-codeArea.value = localStorage.getItem(RULE_KEY) || "";
+codeArea.value = localStorage.getItem(RULE_KEY) || defaultCode;
 codeArea.addEventListener("input", () => {
     localStorage.setItem(RULE_KEY, codeArea.value);
 });
